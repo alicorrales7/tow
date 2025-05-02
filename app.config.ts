06edc6ed5f05ab9e8ca561defc0911cf4e444e70
alicorrales7 <alicorrales2013@gmail.com>
2025-05-02 14:45:52 -0400

    Configuration for Vercel

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,9 @@
+import solid from "solid-start/vite";
+import vercel from "solid-start/vercel";
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   vite: {
-    plugins: []
+    plugins: [solid({ adapter: vercel() })]
   }
 });
